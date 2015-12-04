9b3237996558532544ff9d3e53e516232fcdd159
emilumata <emilumata@hotmail.com>
2015-12-04 00:14:26 -0600

    hw

--- IdentityTemplate/Models/Book.cs
+++ IdentityTemplate/Models/Book.cs
@@ -12,20 +12,31 @@ namespace IdentityTemplate.Models
     {
         [Key]
         public int SKU { get; set; }
+
         public string Title { get; set; }
+
+
+        [Display(Name = "Author First")]
         public string AuthorFirst { get; set; }
+
+        [Display(Name = "Author Last")]
         public string AuthorLast { get; set; }
 
         public string Genre { get; set; }
 
+
+        [Display(Name = "Publication Date ")]
         public DateTime PublicationDate { get; set; }
 
         public decimal Price { get; set; }
 
+        [Display(Name = "Price Last Paid")]
         public decimal PriceLastPaid { get; set; }
 
         public int Inventory { get; set; }
 
+
+        [Display(Name = "Reorder Point")]
         public int ReorderPoint { get; set; }
 
         public bool Discontinued { get; set; }
